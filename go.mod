b111576c1f14d303c32c0ee6ebfecf78408c272e
Ivan Bessarabov <ivan@bessarabov.ru>
2022-05-10 09:59:05 +0300

    Second commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bessarabov/serve
+
+go 1.16
--- serve.go
+++ serve.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func main() {
-	http.ListenAndServe(":3000", http.FileServer(http.Dir(".")))
+
+	port := flag.Int("port", 3000, "Port to run web server to")
+	flag.Parse()
+
+	addr := "127.0.0.1:" + strconv.Itoa(*port)
+	fmt.Println("Listening on http://" + addr)
+	http.ListenAndServe(addr, http.FileServer(http.Dir(".")))
 }
